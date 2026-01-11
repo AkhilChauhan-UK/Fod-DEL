3e75f74d4dd84b49ad3c6a900506ba66db054238
resumeproject <killerhyper89@gmail.com>
2026-01-11 18:00:58 +0530

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://killerhyper89_db_user:akhil123456@cluster0.ngr8bb1.mongodb.net/food-del').then(() => console.log("Db connected"));
+    await mongoose.connect('mongodb+srv://killerhyper89_db_user:ak123456@cluster0.ngr8bb1.mongodb.net/food-del').then(() => console.log("Db connected"));
 }
